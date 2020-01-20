41824b25c947e93e4c0ce49e1aac415a13129b5a
Alexandr-Pyslar <alexandr.pyslar@gmail.com>
2020-01-20 10:46:25 +0300

    0.3 заменил солнце, новые облака, поведение крабов, анимация моря, эффект перспективы

--- Assets/Scripts/MoveCrab.cs
+++ Assets/Scripts/MoveCrab.cs
@@ -2,55 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCrab : MonoBehaviour
-{
+public class MoveCrab : MonoBehaviour {
     private float speed = 1.5f;
     private float waitTime;
-    private float startWaitTime = 1f;
+    private float startWaitTime;
+    private float maxPositionY = -4f;
+    private float minPositionY = -2.43f;
+    private float maxPositionX = 8f;
+    private float minPositionX = -6.5f;
+    private float currentPositionY;
+    public float currentScale;
+    public ParticleSystem stepParticle;
     private Vector2 moveSpot;
     private bool flip = false;
+    private Animator anim;
 
-    void Start()
+    void Start () 
     {
-        moveSpot = new Vector2 (Random.Range(-6.5f, 8f), Random.Range(-2.43f, -4.5f));
-        waitTime = startWaitTime;      
+        moveSpot = new Vector2 (Random.Range (minPositionX, maxPositionX), Random.Range (minPositionY, maxPositionY));
+        startWaitTime = Random.Range (1f, 3f);
+        waitTime = startWaitTime;
+        anim = GetComponent<Animator> ();
     }
 
-    void Update()
+    void Update () 
     {
-        
-        transform.position = Vector2.MoveTowards(transform.position, moveSpot, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards (transform.position, moveSpot, speed * Time.deltaTime);
 
-        if (Vector2.Distance(transform.position,moveSpot)< 0.2f)
-        {
-            
-            if (waitTime <= 0)
-            {
-                moveSpot = new Vector2 (Random.Range(-6.5f, 8f), Random.Range(-2.43f, -4.5f));
+        currentPositionY = transform.position.y;
+
+        if (Vector2.Distance (transform.position, moveSpot) < 0.2f) {
+            if (waitTime <= 0) {
+                moveSpot = new Vector2 (Random.Range (minPositionX, maxPositionX), Random.Range (minPositionY, maxPositionY));
                 waitTime = startWaitTime;
-            } else
-            {
+                anim.SetBool ("isIdle", false);
+                stepParticle.Play ();
+            } else {
                 waitTime -= Time.deltaTime;
+                anim.SetBool ("isIdle", true);
+                stepParticle.Stop ();
                 if (flip) {
-               // moveSpot = new Vector2 (Random.Range(-6.5f, 8f), Random.Range(-2.43f, -4.5f));   
-              //  flip = false;          
                 }
             }
         }
+        ChangeScale ();
     }
 
-
-        void OnTriggerEnter2D(Collider2D other)
-        {        
-           if (other.CompareTag("Crab")) {
-             Debug.Log("Flip!");
-             moveSpot = new Vector2 (Random.Range(-6.5f, 8f), Random.Range(-2.43f, -4.5f));
-                flip = true;
-            } else {
-              //  flip = false;
-            }
+    void OnTriggerEnter2D (Collider2D other) {
+        if (other.CompareTag ("Crab")) {
+            moveSpot = new Vector2 (Random.Range (minPositionX, maxPositionX), Random.Range (minPositionY, maxPositionY));
+            flip = true;
+        } else {
+            flip = false;
         }
+    }
 
-    
+    void ChangeScale () {
+        currentScale = 1.5f + ((currentPositionY / 100) * (100 - (100 / (maxPositionY / currentPositionY))));
+        transform.localScale = new Vector3 (currentScale, currentScale, currentScale);
+    }
 }
-
---
+++ Assets/Scripts/MoveCrabAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCrabAnim : MonoBehaviour
+{
+    private float maxPositionY = -4f;
+    private float minPositionY = -2.43f;
+    private float maxPositionX = 8f;
+    private float minPositionX = -6.5f;
+    private float currentPositionY;
+    public float currentScale;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {         
+        currentPositionY = transform.position.y;
+        ChangeScale();
+    }
+        void ChangeScale()
+        {
+          currentScale = 1.5f  + ((currentPositionY / 100) * (100 - (100 / (maxPositionY / currentPositionY))));
+          transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+        }
+}
+
